090bb07d8709b4fb42cad1876bbd2c8275a68181
victor <victorabelo1697@gmail.com>
2020-05-14 12:54:57 -0300

    Arrow Functions

--- aula07.js
+++ aula07.js
@@ -1,74 +1,61 @@
-// function soma(a, b) {
-//   return a + b;
-// }
+function soma(a, b) {
+  return a + b;
+}
 
-// console.log(soma);
-// console.log(soma(2, 2));
+console.log(soma);
+console.log(soma(2, 2));
 
-// const soma = function (a, b) {
-//   return a + b;
-// }
+const soma = (a, b) => a + b;
 
-// console.log(soma);
-// console.log(soma(2, 2));
+console.log(soma);
+console.log(soma(2, 2));
 
-// const helloWorld = function () {
-//   return function () {
-//     return 'Hello World!';
-//   }
-// }
+const helloWorld = () => () => 'Hello World!';
 
-// console.log(helloWorld);
-// console.log(helloWorld());
-// console.log(helloWorld()());
+console.log(helloWorld);
+console.log(helloWorld());
+console.log(helloWorld()());
 
-// const getIdade = function (extra) {
-//   console.log(arguments);
-//   return this.idade + extra;
-// }
+const getIdade = extra => {
+  console.log(arguments);
+  return this.idade + extra;
+}
 
-// const pessoa = {
-//   name: 'João',
-//   idade: 20,
-//   getIdade: getIdade,
-// };
+const pessoa = {
+  name: 'João',
+  idade: 20,
+  getIdade: getIdade,
+};
 
-// console.log(pessoa.getIdade(2));
-// console.log(getIdade());
-// console.log(getIdade.call(pessoa, 2, 1, 3));
-// console.log(getIdade.apply(pessoa, [2, 1, 3]));
+console.log(pessoa.getIdade(2));
+console.log(getIdade());
+console.log(getIdade.call(pessoa, 2, 1, 3));
+console.log(getIdade.apply(pessoa, [2, 1, 3]));
 
-// Funções Construtoras vs. Funções Fábrica
+// Funções Construtoras vs.Funções Fábrica
 
 // Funções Fábrica
 
-// const criarPessoa = function (nome, idade) {
-//   return {
-//     nome,
-//     idade,
-//   };
-// };
+const criarPessoa = (nome, idade) => { nome, idade };
 
-// console.log(criarPessoa('Pedro', 20));
-// console.log(criarPessoa('Maria', 30));
+console.log(criarPessoa('Pedro', 20));
+console.log(criarPessoa('Maria', 30));
 
-// // Funções Construtoras
+// Funções Construtoras
 
-// class Pessoa {
-//   constructor(nome, idade) {
-//     this.nome = nome;
-//     this.idade = idade;
-//   }
-// }
-
-// console.log(new Pessoa('Pedro', 20));
-// console.log(new Pessoa('Maria', 30));
-
-const helloWorld = function () {
-  const message = 'Hello World!';
-  return function () {
-    return message;
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
   }
+}
+
+console.log(new Pessoa('Pedro', 20));
+console.log(new Pessoa('Maria', 30));
+
+const helloWorld = () => {
+  const message = 'Hello World!';
+  return () => message;
 }
 
 console.log(helloWorld()());
--- aula08.js
+++ aula08.js
@@ -1,61 +1,53 @@
-// let counter = 0;
-// let add = function () {
-//   return ++counter;
-// };
-// console.log(add());
-// console.log(add());
-// console.log(add());
+let counter = 0;
+let add = () => ++counter;
+console.log(add());
+console.log(add());
+console.log(add());
 
-// const itens = [];
-// add = function (item) {
-//   itens.push(item);
-//   return itens;
-// };
-// console.log(add('A'));
-// console.log(add('B'));
-// console.log(add('C'));
-// console.log(add());
+const itens = [];
+add = item => {
+  itens.push(item);
+  return itens;
+};
+console.log(add('A'));
+console.log(add('B'));
+console.log(add('C'));
+console.log(add());
 
-// const counter = {
-//   value: 0,
-//   add: function () {
-//     return this.value++;
-//   }
-// };
-// console.log(counter.add());
-// console.log(counter.add());
-// console.log(counter.add());
+const counter = {
+  value: 0,
+  add: () => this.value++,
+};
+console.log(counter.add());
+console.log(counter.add());
+console.log(counter.add());
 
-// const itens = {
-//   value: [],
-//   add: function (item) {
-//     this.value.push(item);
-//     return this.value;
-//   }
-// };
-// console.log(itens.add('A'));
-// console.log(itens.add('B'));
-// console.log(itens.add('C'));
+const itens = {
+  value: [],
+  add: item => {
+    this.value.push(item);
+    return this.value;
+  }
+};
+console.log(itens.add('A'));
+console.log(itens.add('B'));
+console.log(itens.add('C'));
 
-// console.log(counter.add());
-// counter.value = undefined;
-// console.log(counter.add());
+console.log(counter.add());
+counter.value = undefined;
+console.log(counter.add());
 
-// const createCounter = function () {
-//   let value = 0;
-//   return {
-//     add: function () {
-//       return ++value;
-//     }
-//   };
-// };
-
-const counter = (function () {
+const createCounter = () => {
   let value = 0;
-  const add = function () {
-    return value++;
+  return {
+    add: () => ++value,
   };
-  const reset = function () {
+};
+
+const counter = (() => {
+  let value = 0;
+  const add = () => value++;
+  const reset = () => {
     value = 0;
   };
   return {
